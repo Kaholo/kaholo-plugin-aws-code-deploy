5723990c4be3f259a5687624c0ce4bc0bbaebcc7
xkcm <xkcm16@gmail.com>
2022-04-13 19:48:43 +0200

    Add linting, fix linting errors

--- app.js
+++ app.js
@@ -1,62 +1,64 @@
 const parsers = require("./parsers");
+const CodeDeployService = require("./aws.codeDeploy.service");
+const autocomplete = require("./autocomplete");
 
-const CodeDeployService = require('./aws.codeDeploy.service');
-
-async function createApplication(action, settings){
-    const {name, tags } = action.params;
-    const client = CodeDeployService.from(action.params, settings);
-    return client.createApplication({
-        name: parsers.string(name),
-        tags: parsers.tags(tags)
-    });
+async function createApplication(action, settings) {
+  const { name, tags } = action.params;
+  const client = CodeDeployService.from(action.params, settings);
+  return client.createApplication({
+    name: parsers.string(name),
+    tags: parsers.tags(tags),
+  });
 }
 
-async function createDeploymentGroup(action, settings){
-    const { name, ec2TagFilters, onPremisesInstanceTagFilters, loadBalancingType, application,
-        serviceRole, autoScalingGroups, deploymentConfig, loadBalancer, elbTargetGroup} = action.params;
-    
-    const client = CodeDeployService.from(action.params, settings);
-    return client.createDeploymentGroup({
-        application: parsers.autocomplete(application),
-        name: parsers.string(name),
-        serviceRole: parsers.autocomplete(serviceRole),
-        autoScalingGroups: parsers.array(parsers.autocomplete(autoScalingGroups)),
-        ec2TagFilters: parsers.tags(ec2TagFilters),
-        onPremisesInstanceTagFilters : parsers.tags(onPremisesInstanceTagFilters),
-        deploymentConfig: parsers.autocomplete(deploymentConfig),
-        loadBalancingType: loadBalancingType,
-        loadBalancer: parsers.autocomplete(loadBalancer),
-        elbTargetGroup: parsers.autocomplete(elbTargetGroup)
-    });
+async function createDeploymentGroup(action, settings) {
+  const {
+    name, ec2TagFilters, onPremisesInstanceTagFilters, loadBalancingType, application,
+    serviceRole, autoScalingGroups, deploymentConfig, loadBalancer, elbTargetGroup,
+  } = action.params;
+
+  const client = CodeDeployService.from(action.params, settings);
+  return client.createDeploymentGroup({
+    application: parsers.autocomplete(application),
+    name: parsers.string(name),
+    serviceRole: parsers.autocomplete(serviceRole),
+    autoScalingGroups: parsers.array(parsers.autocomplete(autoScalingGroups)),
+    ec2TagFilters: parsers.tags(ec2TagFilters),
+    onPremisesInstanceTagFilters: parsers.tags(onPremisesInstanceTagFilters),
+    deploymentConfig: parsers.autocomplete(deploymentConfig),
+    loadBalancingType,
+    loadBalancer: parsers.autocomplete(loadBalancer),
+    elbTargetGroup: parsers.autocomplete(elbTargetGroup),
+  });
 }
 
-async function createDeploymentConfig(action, settings){
-    const { name, minHealthyHostsNum, minHealthyHostsPercent } = action.params;
-    
-    const client = CodeDeployService.from(action.params, settings);
-    return client.createDeploymentConfig({
-        name: parsers.string(name),
-        minHealthyHostsNum: parsers.number(minHealthyHostsNum),
-        minHealthyHostsPercent: parsers.number(minHealthyHostsPercent)
-    });
-} 
+async function createDeploymentConfig(action, settings) {
+  const { name, minHealthyHostsNum, minHealthyHostsPercent } = action.params;
 
-async function listApps(action, settings){
-    const client = CodeDeployService.from(action.params, settings);
-    return client.listApps({listAll: true});
-} 
+  const client = CodeDeployService.from(action.params, settings);
+  return client.createDeploymentConfig({
+    name: parsers.string(name),
+    minHealthyHostsNum: parsers.number(minHealthyHostsNum),
+    minHealthyHostsPercent: parsers.number(minHealthyHostsPercent),
+  });
+}
 
-async function listDeploymentsConfigs(action, settings){
-    const client = CodeDeployService.from(action.params, settings);
-    return client.listApps({listAll: true,});
-} 
+async function listApps(action, settings) {
+  const client = CodeDeployService.from(action.params, settings);
+  return client.listApps({ listAll: true });
+}
+
+async function listDeploymentsConfigs(action, settings) {
+  const client = CodeDeployService.from(action.params, settings);
+  return client.listApps({ listAll: true });
+}
 
 module.exports = {
-    createApplication,
-	createDeploymentGroup,
-	createDeploymentConfig,
-    listApps,
-    listDeploymentsConfigs,
-    // Autocomplete Functions
-    ...require("./autocomplete")
-}
+  createApplication,
+  createDeploymentGroup,
+  createDeploymentConfig,
+  listApps,
+  listDeploymentsConfigs,
+  // Autocomplete Functions
+  ...autocomplete,
+};
